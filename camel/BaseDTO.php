595fef5fe5b8cda37247193df23807fa2a054f9b
goktugoner23 <goktug_oner@windowslive.com>
2025-01-16 01:49:19 +0300

    fix: update BaseDTO to use Spatie\LaravelData\Data

--- camel/BaseDTO.php
+++ camel/BaseDTO.php
@@ -3,56 +3,11 @@
 namespace Knuckles\Camel;
 
 use Illuminate\Contracts\Support\Arrayable;
-use Spatie\DataTransferObject\DataTransferObject;
+use Spatie\LaravelData\Data;
 
-
-class BaseDTO extends DataTransferObject implements Arrayable, \ArrayAccess
+class BaseDTO extends Data implements Arrayable, \ArrayAccess
 {
-    /**
-     * @var array $custom
-     * Added so end-users can dynamically add additional properties for their own use.
-     */
     public array $custom = [];
-
-    public static function create(BaseDTO|array $data, BaseDTO|array $inheritFrom = []): static
-    {
-        if ($data instanceof static) {
-            return $data;
-        }
-
-        $mergedData = $inheritFrom instanceof static ? $inheritFrom->toArray() : $inheritFrom;
-
-        foreach ($data as $property => $value) {
-            $mergedData[$property] = $value;
-        }
-
-        return new static($mergedData);
-    }
-
-    protected function parseArray(array $array): array
-    {
-        // Reimplementing here so our DTOCollection items can be recursively toArray'ed
-        foreach ($array as $key => $value) {
-            if ($value instanceof Arrayable) {
-                $array[$key] = $value->toArray();
-
-                continue;
-            }
-
-            if (! is_array($value)) {
-                continue;
-            }
-
-            $array[$key] = $this->parseArray($value);
-        }
-
-        return $array;
-    }
-
-    public static function make(array|self $data): static
-    {
-        return $data instanceof static ? $data : new static($data);
-    }
 
     public function offsetExists(mixed $offset): bool
     {
